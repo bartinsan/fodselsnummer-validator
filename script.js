888c2da16a0f2174aa601ba9658a4ee2ec9c0154
Martin Sandvik <martin@ilder.io>
2020-05-02 23:41:21 +0200

    - Add splittOgSjekkIndividnummer logic and tests. Change all asserts to strictEqual.

--- script.js
+++ script.js
@@ -2,17 +2,54 @@
 // Martin Sandvik
 
 
-function saniterNummer(nummer) {
-    if (nummer.length !== 11) {
-        console.log("Feil: Ugyldig lengde.");
+function saniterNummer(nummerStreng) {
+    if (nummerStreng.length !== 11) {
         return false;
     }
 
-    if (nummer.NaN) {
-        console.log("Feil: Må kun inneholde tall.")
+    if (isNaN(nummerStreng)) {
         return false;
     }
     return true;
+}
+
+function splittOgSjekkIndividnummer(fodselsNrStreng) {
+    // Splitter opp fødselsnummer
+    let dag = fodselsNrStreng.substr(0,2); // Dag
+    let mnd = fodselsNrStreng.substr(2,2); // Måned
+    let ar = fodselsNrStreng.substr(4,2);  // År
+    let arInt = parseInt(ar);                 // År som integer
+    let id = fodselsNrStreng.substr(6,3);
+
+    // Kontrollerer individnummer og år opp mot inndeling
+    if (id > 0 && id <= 499) {                                   // IndividNr 0-499 (1900-1999)
+
+        return true;
+
+    } else if (id >= 500 && id <= 749 && ar >= 54 && ar <= 99) { // IndividNr 500-749 (1854-1899)
+
+        return true;
+
+    } else if (id >= 900 && id <= 999 && ar >= 40 && ar <= 99) { // IndividNr 900-999 (1940-1999)
+
+        return true;
+
+    } else if (id >= 500 && id <= 999 && ar >= 0o0 && ar <= 39) { // IndividNr 500-999 (2000-2039)
+
+        arInt += 2000;
+
+        let fodselsDato = new Date(arInt, mnd-1, dag);
+        fodselsDato.setHours(0,0,0,0,);
+        let dagensDato = new Date();
+        dagensDato.setHours(0,0,0,0);
+
+        // Sjekker om fødselsdato er mindre eller lik dagens dato.
+        return fodselsDato.valueOf() <= dagensDato.valueOf();
+
+    } else {
+
+        return false;
+    }
 }
 
 function validerFodselsnummer(fodselsNr) {
@@ -22,10 +59,19 @@ function validerFodselsnummer(fodselsNr) {
         return false
     }
 
+    // Splitter og sjekker individnummer
+    if (!splittOgSjekkIndividnummer(fodselsNr)) {
+        return false
+    }
+
     return true;
 
 }
 
+if (typeof exports !== 'undefined') {
+    exports.saniterNummer = saniterNummer;
+    exports.splittOgSjekkIndividnummer = splittOgSjekkIndividnummer;
+}
 
 
 
--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ let assert = require("assert");
 let mocha = require("mocha");
 const describe = mocha.describe;
 
-let testF = require('../script');
+let script = require('../script');
 
-describe('-- Testing av logikk for å validere fødselsnummer --', function () {
+describe('-- Testing av logikk i script.js for å validere fødselsnummer --', function () {
 
 
     describe('saniterNummer_tester:', function() {
@@ -12,21 +12,91 @@ describe('-- Testing av logikk for å validere fødselsnummer --', function () {
         describe('Fødselsnummer er ikke 11 siffer langt', function () {
             let testNummer = '111111111112';
             it('Bør returnere "false" når fødselsnummer ikke er 11 siffer langt', function () {
-                assert.equal(testF.saniterNummer(testNummer), false);
+                assert.strictEqual(script.saniterNummer(testNummer), false);
             });
         });
 
         describe('Fødselsnummer er 11 karakterer langt, men er ikke et nummer.', function () {
             let testNummer = '1111111111a';
-            it('Bør returnere "false" når fødselsnummer er 11 karakterer langt', function () {
-                assert.equal(testF.saniterNummer(testNummer), false);
+            it ('Bør returnere "false" når fødselsnummer er 11 karakterer langt', function () {
+                assert.strictEqual(script.saniterNummer(testNummer), false);
             });
         });
 
         describe('Fødselsnummer er et nummer og er 11 siffer langt', function () {
             let testNummer = '11111111111';
             it('Bør returnere "false" når fødselsnummer er et nummer og er 11 siffer langt.', function () {
-                assert.equal(testF.saniterNummer(testNummer), true);
+                assert.strictEqual(script.saniterNummer(testNummer), true);
+            });
+        });
+
+    });
+
+    describe('splittOgSjekkIndividnummer_tester', function () {
+
+        describe('Individnummer er større enn 0 og mindre enn eller lik 499', function () {
+            let testNummer = '11111122211';
+            it('Bør returnere "true" når individnummer er større en 0 og mindre enn eller lik 499', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), true);
+            });
+        });
+
+        describe('Individnummer er større enn eller lik 500 og mindre enn eller lik 749,' +
+            ' og fødselsår er større enn eller lik 54 og mindre enn eller lik 99.', function () {
+            let testNummer = '11116670011';
+            it('Bør returnere "true" når individnummer er større enn eller lik 500 og mindre enn eller lik 749, ' +
+                'og fødselsår er større enn eller lik 54 og mindre enn eller lik 99', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), true);
+            });
+        });
+
+        describe('Individnummer er større enn eller lik 900 og mindre eller lik 999, ' +
+            'og fødselsår er større enn eller lik 40 og mindre enn eller lik 99.', function () {
+            let testNummer = '11116695011';
+            it('Bør returnere "true" når individnummer er større enn eller lik 900 og mindre eller lik 999, ' +
+                'og fødselsår er større enn eller lik 40 og mindre enn eller lik 99.', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), true);
+            });
+        });
+
+        describe('Individnummer er større enn eller lik 500 og mindre enn eller lik 999, ' +
+            'og fødselsår er større enn eller lik 00 og mindre enn eller lik 39, ' +
+            'og fødselsdato er ikke høyere enn dagens dato.', function () {
+            let testNummer = '0105205011';
+            it('Bør returnere "true" når individnummer er større enn eller lik 500 og mindre enn eller lik 999, ' +
+                'og fødselsår er større enn eller lik 00 og mindre enn eller lik 39,' +
+                ' og fødselsdato ikke høyere enn dagens dato.', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), true);
+            });
+        });
+
+        describe('Individnummer er større enn eller lik 500 og mindre enn eller lik 999, og fødselsår er større enn eller lik 00 og mindre enn eller lik 39, ' +
+            ' men fødselsdato er høyere enn dagens dato.', function () {
+
+            let testDato = new Date();
+            testDato.setDate(testDato.getDate()+1);
+            let testDatoArray = testDato.toLocaleDateString('en-GB').split('/');
+            let testDatoStreng = ('0' + testDatoArray[1]).slice(-2) + ('0' + testDatoArray[0]).slice(-2) + testDatoArray[2].slice(-2);
+
+            let testNummer = testDatoStreng  + '66611';
+
+            it('Bør returnere "false" når fødselsdato er større enn dagens dato', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), false);
+            });
+
+        });
+
+        describe('Individnummeret er større enn eller lik 500 og mindre enn eller lik 749, men fødselsår er mindre enn 54', function () {
+            let testNummer = '01012152011';
+            it('Bør returnere "false" når individnummer er større enn eller lik 500 og mindre enn eller lik 749, men fødselsår er mindre enn 54', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), false);
+            });
+        });
+
+        describe('Individnummeret er større enn eller lik 900 og mindre enn eller lik 999, men fødselsår er mindre enn 40', function () {
+            let testNummer = '01012152011';
+            it('Bør returnere "false" når individnummer er større enn eller lik 500 og mindre enn eller lik 749, men fødselsår er mindre enn 54', function () {
+                assert.strictEqual(script.splittOgSjekkIndividnummer(testNummer), false);
             });
         });
 
