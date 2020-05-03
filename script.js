3b4a4210f0aa330d2beb27ed32e02695b48a30cf
Martin Sandvik <martin@ilder.io>
2020-05-03 02:48:29 +0200

    - Add UI-logic to script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // Dette er et Script som tester og validerer Norske fødselsnummer.
 // Martin Sandvik
 
+let form = document.getElementById("ui-form");
+function handleForm(event) {
+    event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
+
+function setUIStatus(klasse, melding) {
+
+    let statusOmrade = document.querySelector('.status-omrade');
+    let nyttElement = document.createElement('p');
+    nyttElement.innerHTML = melding;
+    nyttElement.className = klasse;
+
+    if (statusOmrade.hasChildNodes()) {
+        while (statusOmrade.hasChildNodes()){
+            statusOmrade.removeChild(statusOmrade.lastChild);
+
+        }
+    }
+
+    statusOmrade.appendChild(nyttElement);
+
+}
+
+function valider(fodselsNrStreng) {
+    if (!validerFodselsnummer(fodselsNrStreng)) {
+        setUIStatus('error', '\u2612 Ugyldig fødselsnummer. Prøv igjen!');
+    } else {
+        setUIStatus('info', '\u2611 Gyldig fødselsnummer!')
+    }
+}
 
 function saniterNummer(nummerStreng) {
     if (nummerStreng.length !== 11) {
