2b2f53bf4d3b286869a45bdf059e7fa922cc218c
Martin Sandvik <martin@ilder.io>
2020-05-04 02:06:00 +0200

    Revert "- Simplify server logic"
    This reverts commit 892d58b8

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+
+});
+
 const app = express();
+
+server.on('request', app);
+app.use(express.static('./'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => console.log(`Server running on port 3000!`));
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
